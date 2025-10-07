60ea4defefbd9feb282f3e3a93af1ff3370638d3
sergio_from_tauri <36307887+SerhiiZahuba@users.noreply.github.com>
2025-10-07 13:56:31 +0300

    Delete api directory

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from netbox.api.routers import NetBoxRouter
-from . import views
-
-router = NetBoxRouter()
-router.register('configs', views.CloudSyncConfigViewSet)
-
-urlpatterns = router.urls
--- api/views.py
+++
@@ -1,8 +0,0 @@
-from netbox.api.viewsets import NetBoxModelViewSet
-from ..models import CloudSyncConfig
-from .serializers import CloudSyncConfigSerializer
-
-
-class CloudSyncConfigViewSet(NetBoxModelViewSet):
-    queryset = CloudSyncConfig.objects.all()
-    serializer_class = CloudSyncConfigSerializer
